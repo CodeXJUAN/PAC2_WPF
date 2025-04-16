2e87552f32db984c2f4e5aeb3b11582da7e15380
Juan Manuel López Arrieta <juanmanuellopezarrieta@gmail.com>
2025-04-16 16:35:21 +0200

    dni textbox funciona completamente falta importarlo en el proyecto de la pac1

--- CustomControlsLib/DNITextBox.xaml.cs
+++ CustomControlsLib/DNITextBox.xaml.cs
@@ -1,38 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
+using System.Globalization;
 
 namespace CustomControlsLib
 {
     public partial class DNITextBox : UserControl
     {
-        // Regular expression to validate DNI format (8 digits + 1 letter)
-        private static readonly Regex DniRegex = new Regex(@"^\d{8}[A-Za-z]$");
-
-        // Letters for DNI validation
+        private static readonly Regex DniRegex = new(@"^\d{8}[A-Za-z]$");
         private static readonly string ValidLetters = "TRWAGMYFPDXBNJZSQVHLCKE";
 
         public DNITextBox()
         {
             InitializeComponent();
-            // Default color will be gray until user starts typing
-            BorderColor = Brushes.Gray;
         }
 
         #region Dependency Properties
-
         public static readonly DependencyProperty DNIValueProperty =
             DependencyProperty.Register("DNIValue", typeof(string), typeof(DNITextBox),
                 new PropertyMetadata(string.Empty, OnDNIValueChanged));
@@ -41,93 +27,109 @@ namespace CustomControlsLib
             DependencyProperty.Register("IsValid", typeof(bool), typeof(DNITextBox),
                 new PropertyMetadata(false));
 
-        public static readonly DependencyProperty BorderColorProperty =
-            DependencyProperty.Register("BorderColor", typeof(Brush), typeof(DNITextBox),
-                new PropertyMetadata(Brushes.Gray));
+        public static readonly DependencyProperty TextColorProperty =
+            DependencyProperty.Register("TextColor", typeof(Brush), typeof(DNITextBox),
+                new PropertyMetadata(Brushes.Black));
 
+        public static readonly DependencyProperty ErrorMessageProperty =
+            DependencyProperty.Register("ErrorMessage", typeof(string), typeof(DNITextBox),
+                new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty ShowErrorMessageProperty =
+            DependencyProperty.Register("ShowErrorMessage", typeof(bool), typeof(DNITextBox),
+                new PropertyMetadata(false));
         #endregion
 
         #region Properties
-
         public string DNIValue
         {
-            get { return (string)GetValue(DNIValueProperty); }
-            set { SetValue(DNIValueProperty, value); }
+            get => (string)GetValue(DNIValueProperty);
+            set => SetValue(DNIValueProperty, value);
         }
 
         public bool IsValid
         {
-            get { return (bool)GetValue(IsValidProperty); }
-            private set { SetValue(IsValidProperty, value); }
+            get => (bool)GetValue(IsValidProperty);
+            private set => SetValue(IsValidProperty, value);
         }
 
-        public Brush BorderColor
+        public Brush TextColor
         {
-            get { return (Brush)GetValue(BorderColorProperty); }
-            private set { SetValue(BorderColorProperty, value); }
+            get => (Brush)GetValue(TextColorProperty);
+            private set => SetValue(TextColorProperty, value);
         }
 
-        #endregion
+        public string ErrorMessage
+        {
+            get => (string)GetValue(ErrorMessageProperty);
+            set => SetValue(ErrorMessageProperty, value);
+        }
 
-        #region Event Handlers
+        public bool ShowErrorMessage
+        {
+            get => (bool)GetValue(ShowErrorMessageProperty);
+            set => SetValue(ShowErrorMessageProperty, value);
+        }
+        #endregion
 
         private static void OnDNIValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if (d is DNITextBox control)
-            {
-                control.ValidateDNI();
-            }
+            if (d is DNITextBox control) control.ValidateDNI();
         }
-
-        #endregion
-
-        #region Methods
 
         private void ValidateDNI()
         {
-            bool isValid = false;
-
-            // If empty, set to default gray
             if (string.IsNullOrEmpty(DNIValue))
             {
-                BorderColor = Brushes.Gray;
+                TextColor = Brushes.Red;
                 IsValid = false;
+                ErrorMessage = "DNI no puede estar vacío";
+                ShowErrorMessage = true;
                 return;
             }
 
-            // Check if the format is valid (8 digits + 1 letter)
-            if (DniRegex.IsMatch(DNIValue))
+            if (DNIValue.Length > 0 && DNIValue.Length != 9)
             {
-                // Always validate the check letter
-                isValid = IsCheckLetterValid();
+                TextColor = Brushes.Red;
+                IsValid = false;
+                ErrorMessage = "El DNI debe tener 9 caracteres (8 números + 1 letra)";
+                ShowErrorMessage = true;
+                return;
             }
 
-            // Update IsValid property
-            IsValid = isValid;
+            if (DniRegex.IsMatch(DNIValue))
+            {
+                bool isValidLetter = IsCheckLetterValid();
+                TextColor = isValidLetter ? Brushes.Green : Brushes.Red;
+                IsValid = isValidLetter;
 
-            // Update border color based on validation
-            BorderColor = isValid ? Brushes.Green : Brushes.Red;
+                if (!isValidLetter)
+                {
+                    ErrorMessage = "La letra de control no es válida";
+                    ShowErrorMessage = true;
+                }
+                else
+                {
+                    ShowErrorMessage = false;
+                }
+            }
+            else
+            {
+                TextColor = Brushes.Red;
+                IsValid = false;
+                ErrorMessage = "Formato de DNI inválido. Debe ser 8 números seguidos de una letra";
+                ShowErrorMessage = true;
+            }
         }
 
         private bool IsCheckLetterValid()
         {
-            if (DNIValue.Length != 9)
-                return false;
+            if (DNIValue.Length != 9) return false;
+            if (!int.TryParse(DNIValue[..8], out int dniNumber)) return false;
 
-            // Extract number and letter
-            if (!int.TryParse(DNIValue.Substring(0, 8), out int dniNumber))
-                return false;
-
-            // Get the provided check letter (convert to uppercase)
             char providedLetter = char.ToUpper(DNIValue[8]);
-
-            // Calculate the correct check letter
-            int remainder = dniNumber % 23;
-            char correctLetter = ValidLetters[remainder];
-
-            // Compare the letters
-            return correctLetter == providedLetter;
+            return ValidLetters[dniNumber % 23] == providedLetter;
         }
-
-        #endregion
     }
+
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,28 +1,14 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp1;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace WpfApp1
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
-    }
-
-    private void EmailTextBox_Loaded()
-    {
-
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
     }
 }
